02cc7c7fd2cb831435aaedda77440e587eb610d7
Darryl Anderson <6097929+darrylanderson@users.noreply.github.com>
2018-02-20 13:11:59 -0600

    Initial import of spring boot admin server

---
+++ src/main/java/atc/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,21 @@
+package atc.springbootadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication
+{
+
+    public static void main ( final String[] args )
+    {
+        SpringApplication.run( SpringBootAdminApplication.class, args );
+    }
+
+}
